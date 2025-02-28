d31a02f6bbe76bd1ba010bce57fb38e3a022f34c
arthureastwood <arthurpenragon@gmail.com>
2025-02-28 16:49:15 +0300

    Added calculator logic
    The logic is what makes the calc operational

---
+++ script.js
@@ -0,0 +1,87 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+document.addEventListener('DOMContentLoaded', function(){
+    //Store all elements in HTML in our JS
+    let clear = document.querySelector(".clear");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent);
+        currentScreen.textContent = currentValue;
+    }));
+
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent);
+        previousScreen.textContent = previousValue + "" + operator;
+        currentScreen.textContent = currentValue;
+    }));
+
+    clear.addEventListener("click", function(){
+        previousValue = '';
+        currentValue = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    });
+
+    equal.addEventListener("click", function(){
+        if(currentValue !== '' && previousValue !== ''){
+            operate();
+            previousScreen.textContent = '';
+            currentScreen.textContent = previousValue;
+        }     
+    });
+
+    decimal.addEventListener("click", addDecimal);
+});
+
+function handleNumber(num){
+    if(currentValue.length < 10){
+        currentValue += num;
+    }
+    
+}
+
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
+
+function operate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue;
+    }
+    else if(operator === "-"){
+        previousValue -= currentValue;
+    }
+    else if(operator === "x"){
+        previousValue *= currentValue;
+    }
+    else{
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += '.';
+    }
+}
